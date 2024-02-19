901726eba3eda706f1f4ede3cd7d4cc36832f8f4
jahidmollah <jahidmollah1212@gmail.com>
2024-02-19 21:13:22 +0530

    resolve merge

--- app.py
+++ app.py
@@ -4,13 +4,8 @@ def addition(x,y,e):
     return x+y+e
 
 
-<<<<<<< HEAD
 def subsraction(x,y,f):
     return x-y-f
-=======
-def subsraction(x,y,g):
-    return x-y-g
->>>>>>> 3877f58ec0afca9c76d058c0aae93eb915013344
 
 
 def divission(a,b):
